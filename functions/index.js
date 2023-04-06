3c4b8c25795e0a1d87f3de4673e7fa20bda5269a
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-04-05 20:22:12 -0700

    new changes

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,18 @@
 const functions = require("firebase-functions");
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+
+exports.addMessage() = functions.https.onRequest(async(req,res)=> {
+    const original = req.query.text;
+    
+    const writeResult 
+})
+
+
+
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //

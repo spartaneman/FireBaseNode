1a08e07f5c230554ea995b466e6a61872da3e0de
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-04-05 20:22:20 -0700

    more

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,11 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 
-exports.addMessage() = functions.https.onRequest(async(req,res)=> {
-    const original = req.query.text;
+// exports.addMessage() = functions.https.onRequest(async(req,res)=> {
+//     const original = req.query.text;
     
-    const writeResult 
-})
+//     const writeResult 
+// })
 
 
 
@@ -16,7 +16,7 @@ exports.addMessage() = functions.https.onRequest(async(req,res)=> {
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
